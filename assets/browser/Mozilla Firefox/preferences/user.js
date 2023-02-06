936fa50f39e056df01c162d4ecf7151ca1f4f22b
danmacode <danmacode1@gmail.com>
2023-02-06 12:49:04 -0800

    Add assets

---
+++ assets/browser/Mozilla Firefox/preferences/user.js
@@ -0,0 +1,10 @@
+/**  Mozilla User Preferences
+ * 
+*/
+
+// browser.region.network.url
+user_pref("browser.region.network.url", "");
+user_pref("browser.region.update.enabled", false);
+// more language defaults
+user_pref("intl.accept_languages", "en-US, en, fr");
+user_pref("javascript.use_us_english_locale", true);
